32db1c94ef75b6281749828a8c9a9c0102b78e9e
bayoslav <gaming4ever93@gmail.com>
2018-05-10 22:22:42 +0200

    Fixed the superuser bug

--- authentication/models.py
+++ authentication/models.py
@@ -1,18 +1,16 @@
 from django.db import models
 
 # Create your models here.
+#from django.contrib.auth.models import 
 
-from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager,PermissionsMixin
 #from datetime import datetime
 from django.utils import timezone as timezone
 # Create your models here.
 class UserManager(BaseUserManager):
-    def create_user(self, email,username,dada=None,password=None, **kwargs):
+    def create_user(self, email,username,password=None,dada=None, **kwargs):
         # Ensure that an email address is set
-        print(email)
-        print(username)
-        print(dada)
-        print(password)
+       
         if not email:
             raise ValueError('Users must have a valid e-mail address')
         # Ensure that a username is set
@@ -28,22 +26,22 @@ class UserManager(BaseUserManager):
         return user
 
     def create_superuser(self, username, email, password=None, **kwargs):
-        print(username)
-        print(email)
-        print(password)
+        
         #username = kwargs.get('username')
         user = self.create_user(email, username,password, kwargs)
         user.is_admin = True
         user.is_staff = True
+        user.is_superuser = True 
         user.save()
 
         return user
 
-class User(AbstractBaseUser):
+class User(AbstractBaseUser, PermissionsMixin):
     username = models.CharField(unique=True, max_length=60)
     email = models.EmailField(unique=True) 
     is_active = models.BooleanField(default=True)
     is_admin = models.BooleanField(default=False)
+    is_staff = models.BooleanField(default=False)
     date_created = models.DateTimeField(default=timezone.now)
     objects = UserManager()
     USERNAME_FIELD = 'username'
