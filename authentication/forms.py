33e261a16f5c44941c11804ad1b4a07ee1da1544
Filip Lav Maksimovic <gaming4ever93@gmail.com>
2018-05-11 21:26:25 +0200

    Made views and templates

---
+++ authentication/forms.py
@@ -0,0 +1,25 @@
+from django import forms
+from .models import User
+
+class RegForm(forms.ModelForm):
+    username = forms.CharField(required=True, widget=forms.TextInput(attrs={}))
+    #url = forms.URLField(label='Your website', required=False)
+    email = forms.CharField(required=True, widget=forms.TextInput(attrs={}))
+    password = forms.CharField(required=True, min_length=8, widget=forms.PasswordInput(attrs={}))
+    def save(self, commit=True):
+        user = super(RegForm, self).save(commit=False)
+        user.set_password(self.cleaned_data["password"])
+        if commit:
+            user.save()
+        return user
+    class Meta:
+        model = User
+        fields = ("username", "email", "password")
+
+class LoginForm(forms.Form):
+    username = forms.CharField(required=True, widget=forms.TextInput(attrs={}))
+    #url = forms.URLField(label='Your website', required=False)
+    password = forms.CharField(required=True, widget=forms.PasswordInput(attrs={}))
+    class Meta:
+
+        fields = ("username", "password")
