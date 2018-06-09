b332faa45ff9cfd940533d5a011870322faa628c
Filip Lav Maksimovic <gaming4ever93@gmail.com>
2018-06-09 22:14:46 +0200

    Update forms.py

--- authentication/forms.py
+++ authentication/forms.py
@@ -3,7 +3,6 @@ from .models import User
 
 class RegForm(forms.ModelForm):
     username = forms.CharField(required=True, widget=forms.TextInput(attrs={}))
-    #url = forms.URLField(label='Your website', required=False)
     email = forms.CharField(required=True, widget=forms.TextInput(attrs={}))
     password = forms.CharField(required=True, min_length=8, widget=forms.PasswordInput(attrs={}))
     def save(self, commit=True):
@@ -18,7 +17,6 @@ class RegForm(forms.ModelForm):
 
 class LoginForm(forms.Form):
     username = forms.CharField(required=True, widget=forms.TextInput(attrs={}))
-    #url = forms.URLField(label='Your website', required=False)
     password = forms.CharField(required=True, widget=forms.PasswordInput(attrs={}))
     class Meta:
 
